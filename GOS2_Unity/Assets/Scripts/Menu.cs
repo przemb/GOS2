68e63f6def434b4b4b69ffe4435ecf37fead38bf
Marrkix <MarrkixPL@gmail.com>
2017-09-23 13:30:20 +0200

    mar

---
+++ GOS2_Unity/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+    
+
+    public void StartPressed ()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+	public void ExitPressed ()
+	{
+		Application.Quit();
+		UnityEditor.EditorApplication.isPlaying = false;
+	}
+
+}
