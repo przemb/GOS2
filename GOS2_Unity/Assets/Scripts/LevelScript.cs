67088f77de438adf77950ea378786948632ddc57
Marrkix <MarrkixPL@gmail.com>
2017-09-24 13:22:37 +0200

    return to menu and menu greeting

--- GOS2_Unity/Assets/Scripts/LevelScript.cs
+++ GOS2_Unity/Assets/Scripts/LevelScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelScript : MonoBehaviour {
 
@@ -38,6 +39,10 @@ public class LevelScript : MonoBehaviour {
     
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetKey ("escape"))
+			SceneManager.LoadScene ("MenuScene");
+
         if(!ac.audio.isPlaying && !finished && !gestureController.spellController.blockade)
         {
             switch (audioState)
