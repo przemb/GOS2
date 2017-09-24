eb8b3c0c716205b19901167ee1de21588d9c30ca
Marrkix <MarrkixPL@gmail.com>
2017-09-24 16:44:38 +0200

    porady

--- GOS2_Unity/Assets/Scripts/Menu.cs
+++ GOS2_Unity/Assets/Scripts/Menu.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
+	int n = 0;
+
+	public AudioClip[] porady;
+
+	public AudioSource audioSource;
 
     // Use this for initialization
     void Start () {
@@ -33,6 +38,16 @@ public class Menu : MonoBehaviour {
     {
         SceneManager.LoadScene("MenuScene");
     }
+
+	public void HelpPressed()
+	{
+		audioSource.clip = porady [n];
+		audioSource.Play ();
+
+		++n;
+		if (n > 1)
+			n = 0;
+	}
 
 
 
